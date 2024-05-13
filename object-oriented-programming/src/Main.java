aed76a65188e448aad2da34c693e92b2de783a19
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-05-13 11:02:30 -0300

    Testing packages

--- object-oriented-programming/src/Main.java
+++ object-oriented-programming/src/Main.java
@@ -1,5 +1,15 @@
+import com.mastersoft.clinical.model.Exame;
+import com.mastersoft.clinical.service.ExameService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Exame barrosExam = new Exame();
+
+        barrosExam.id = 1;
+
+        ExameService exameService = new ExameService();
+
+        exameService.salvarExame(barrosExam);
     }
 }
